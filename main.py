807c7f56115f13a5ed84f84aa2606faaa27e598a
Sanjay-D-works <Sanjaydevarajworks@gmail.com>
2025-05-20 22:51:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ class FoodApp:
     @Staticmethod
     def INIT():#Initial Method
         print("<< Welcome to Online Food Ordering system >>")
+    
+    def __init__(self,login,signup):
+        
         
